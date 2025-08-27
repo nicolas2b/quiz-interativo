28d6243f2575e839bee2c7e5aaefa23b4b5162ae
nicolas <santos.moraes.nicolas@escola.pr.gov.br>
2025-08-27 07:43:51 -0300

    .

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,99 @@
+const perguntas = [
+    {
+        enunciado: "pergunta 1", 
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1", 
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1", 
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1", 
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            },
+           {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "pergunta 1", 
+        alternativas: [
+            {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ]
+            },
+           {
+                texto: "afirmativa 1",
+                afirmação: [
+                    "afirmação 1",
+                    "afirmação 2"
+                ]
+            }
+        ]
+    },
+]
+
 
